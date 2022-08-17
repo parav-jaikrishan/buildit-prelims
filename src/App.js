4abfe4e24aa720d37a919bbd4862b01a18c82d0c
parav-jaikrishan <r21947parav@dpsrkp.net>
2022-08-18 04:06:21 +0530

    Linked to shopping page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Home } from './pages/Home';
 import { About } from './pages/About';
+import { Shopping } from './pages/Shopping';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
+        <Route path="/shop" element={<Shopping/>}/>
       </Routes>
     </Router>
   );
