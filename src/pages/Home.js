04bd50028a886e7c991a79e38c633c98e84dbc21
parav-jaikrishan <r21947parav@dpsrkp.net>
2022-08-18 02:34:08 +0530

    Fixed styling issue and updated home content section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { Header } from "../components/header/Header";
+import { LaptopNavbar } from "../components/laptop-navbar/LaptopNavbar";
+import { HomeContent } from "../components/home-content/HomeContent";
+import "./style.scss";
 
 export const Home = () => {
     return (
         <div>
             <Header/>
+            <div className="content-container">
+                <LaptopNavbar/>
+                <HomeContent/>
+            </div>
         </div>
     );
 }
