785e50f460bfc23ae781c2068f4e9816d25e801e
Parav Jaikrishan <r21947parav@dpsrkp.net>
2022-08-18 01:13:11 +0530

    Completed header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.scss";
 import pokemonLogo from "../../assets/pokemonLogo.png";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
+    const [mobileNavActive, setMobileNavActive] = useState("");
+    const openMobileNav = () => {
+        mobileNavActive === "" ? setMobileNavActive("active") : setMobileNavActive("");
+    }
     return (
         <div>
-            <div className="mobile-navbar">
-                <div className="hamburger">
+            <div className="mobile-header">
+                <div className= { "hamburger " + mobileNavActive } onClick={() => openMobileNav()}>
                     <span className="bar"></span>
                     <span className="bar"></span>
                     <span className="bar"></span>
                 </div>
                 <img src={pokemonLogo} alt="Pokemon logo"/>
-                <div className="links">
-                    
+                <div className={ "links " + mobileNavActive }>
+                    <Link className="link" to="/">Home</Link>
+                    <Link className="link" to="/">Info</Link>
+                    <Link className="link" to="/">Shop</Link>
+                    <Link className="link" to="/">Maps</Link>
                 </div>
+            </div>
+            <div className="laptop-header">                
+                <img src={pokemonLogo} alt="Pokemon logo"/>
             </div>
         </div>
     );
