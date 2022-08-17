e927bb0b9076fb8d78deebc96aea969232b83c2b
Parav Jaikrishan <r21947parav@dpsrkp.net>
2022-08-18 00:47:23 +0530

    Updated to implement routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import { Home } from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+      </Routes>
+    </Router>
   );
 }
 
