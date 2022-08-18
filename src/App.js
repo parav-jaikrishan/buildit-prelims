56bebfcca0c2d65a8b8071b9b2348fb349529483
parav-jaikrishan <r21947parav@dpsrkp.net>
2022-08-18 14:58:23 +0530

    Linked to maps page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { Shopping } from './pages/Shopping';
+import { Maps } from './pages/Maps';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route exact path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/shop" element={<Shopping/>}/>
+        <Route path="/maps" element={<Maps/>}/>
       </Routes>
     </Router>
   );
