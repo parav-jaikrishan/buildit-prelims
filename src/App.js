1459a8c55600ac8d8c98b42f1bcde30888c5b5c8
Parav Jaikrishan <r21947parav@dpsrkp.net>
2022-08-18 15:35:35 +0530

    Linked to Gyms page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { Shopping } from './pages/Shopping';
 import { Maps } from './pages/Maps';
+import { Gyms } from './pages/Gyms';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/about" element={<About/>}/>
         <Route path="/shop" element={<Shopping/>}/>
         <Route path="/maps" element={<Maps/>}/>
+        <Route path="/gyms" element={<Gyms/>}/>
       </Routes>
     </Router>
   );
