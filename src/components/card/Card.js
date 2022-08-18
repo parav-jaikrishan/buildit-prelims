bbe858153322219b6cba6dce9432558d70a2e9de
parav-jaikrishan <r21947parav@dpsrkp.net>
2022-08-18 14:48:27 +0530

    Fixed bug

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -4,7 +4,7 @@ import "./Card.scss";
 export const Card = (props) => {
   return (
       <div onClick={props.clicked} className="card" >
-        <img src={props.image} />
+        <img src={props.image} alt={props.name} />
         <p>{props.name}</p>
       </div>
   );
