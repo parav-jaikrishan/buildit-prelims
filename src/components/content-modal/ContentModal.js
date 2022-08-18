9acdcbaf95d51ea263b3ff52056e4169cfd1b810
parav-jaikrishan <r21947parav@dpsrkp.net>
2022-08-18 14:27:49 +0530

    Created content modal

--- src/components/content-modal/ContentModal.js
+++ src/components/content-modal/ContentModal.js
@@ -4,9 +4,12 @@ import "./ContentModal.scss";
 export const ContentModal = props => {
     const { data, show } = props;
     return (
-        <div className={`content-modal ${show}`}>
+        <div className={`content-modal ${show}`} onClick={props.onClose}>
             <div className="modal-content">
                 <span className="close" onClick={props.onClose}>&times;</span>
+                <img className="shop-img" src={data.img} alt={data.name}/>
+                <h1>{data.name}</h1>
+                <p>{data.content}</p>
             </div>
         </div>
     );
