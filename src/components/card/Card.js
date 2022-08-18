5621a834adf7e7d545326b160822d3a8b63dea7a
parav-jaikrishan <r21947parav@dpsrkp.net>
2022-08-18 12:05:35 +0530

    Card component edited to make it easily customizable

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,45 +1,11 @@
 import React from 'react';
 import "./Card.scss";
-import bulbasaur from "../../assets/bulbasaur.png";
-import image1 from "../../assets/031.png";
-import image2 from "../../assets/591.png";
-import rowlet from "../../assets/rowlet.png";
 
-export const Card = () => {
+export const Card = (props) => {
   return (
-    <div className="card-container">
       <div className="card" >
-          <div className="card-body">
-            <img src={bulbasaur} />
-          </div>
-          <div className="card-footer small">
-            Bulbasaur
-          </div>
+        <img src={props.image} />
+        <p>{props.name}</p>
       </div>
-      <div className="card" >
-          <div className="card-body">
-            <img src={image1}/>
-          </div>
-          <div className="card-footer small">
-            Pokemon
-          </div>
-      </div>
-      <div className="card" >
-          <div className="card-body">
-            <img src={image2} />
-          </div>
-          <div className="card-footer small">
-            Bulbasaur
-          </div>
-      </div>
-      <div className="card" >
-          <div className="card-body">
-            <img src={rowlet}/>
-          </div>
-          <div className="card-footer small">
-            Rowlet
-          </div>
-      </div>
-    </div>
   );
 }
--- src/components/shopping-content/ShoppingContent.js
+++ src/components/shopping-content/ShoppingContent.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import { Card } from '../card/Card';
 import "./ShoppingContent.scss";
+import bulbasaur from "../../assets/bulbasaur.png";
+import nidoqueen from "../../assets/nidoqueen.png";
+import groudon from "../../assets/groudon.png";
+import rowlet from "../../assets/rowlet.png";
 
 export const ShoppingContent = () => {
+    const shoppingCards = [
+        {
+            name: 'Bulbasaur',
+            img: bulbasaur,
+        },
+        {
+            name: 'Nidoqueen',
+            img: nidoqueen,
+        },
+        {
+            name: 'Groudon',
+            img: groudon,
+        },
+        {
+            name: 'Rowlet',
+            img: rowlet,
+        }
+    ];
     return (
         <div className="shopping-content">
             <h1>Shop</h1>
-            <Card />
+            <div className="card-container">
+                {shoppingCards.map(card => {
+                    return (
+                        <Card name={card.name} image={card.img}/>
+                    );
+                })}
+            </div>
         </div>
     );
 }
