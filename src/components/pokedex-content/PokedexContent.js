3967aa41b4987dc3a621ce0984b2433c38f6364c
parav-jaikrishan <r21947parav@dpsrkp.net>
2022-08-21 03:03:05 +0530

    Stats added to pokedex

--- src/components/pokedex-content/PokedexContent.js
+++ src/components/pokedex-content/PokedexContent.js
@@ -63,9 +63,9 @@ export const PokedexContent = () => {
             color: 'brown',
             content: `Emerged from the prehistoric walls of the renown minaret, The Qutub Minar, Qutubex is one of the specialties of Pokemon's New Delhi Centre`,
             stats: {
-                hp: 50,
-                attack: 50,
-                defense: 50
+                hp: 75,
+                attack: 83,
+                defense: 78
             }
         },
         {
@@ -74,9 +74,9 @@ export const PokedexContent = () => {
             color: 'green',
             content: `Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun's rays, the seed grows progressively larger.`,
             stats: {
-                hp: 50,
-                attack: 50,
-                defense: 50
+                hp: 20,
+                attack: 20,
+                defense: 20
             }
         },
         {
@@ -85,9 +85,9 @@ export const PokedexContent = () => {
             color: 'green',
             content: `There is a bud on this Pokémon's back. To support its weight, Ivysaur's legs and trunk grow thick and strong. If it starts spending more time lying in the sunlight, it's a sign that the bud will bloom into a large flower soon.`,
             stats: {
-                hp: 50,
-                attack: 50,
-                defense: 50
+                hp: 30,
+                attack: 35,
+                defense: 30
             }
         },
         {
@@ -96,9 +96,9 @@ export const PokedexContent = () => {
             color: 'green',
             content: `There is a large flower on Venusaur's back. The flower is said to take on vivid colors if it gets plenty of nutrition and sunlight. The flower's aroma soothes the emotions of people.`,
             stats: {
-                hp: 50,
-                attack: 50,
-                defense: 50
+                hp: 40,
+                attack: 45,
+                defense: 35
             }
         },
         {
@@ -107,9 +107,9 @@ export const PokedexContent = () => {
             color: 'orange',
             content: `It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.`,
             stats: {
-                hp: 50,
-                attack: 50,
-                defense: 50
+                hp: 20,
+                attack: 30,
+                defense: 20
             }
         },
         {
@@ -118,9 +118,9 @@ export const PokedexContent = () => {
             color: 'orange',
             content: `It has a barbaric nature. In battle, it whips its fiery tail around and slashes away with sharp claws.`,
             stats: {
-                hp: 50,
-                attack: 50,
-                defense: 50
+                hp: 30,
+                attack: 30,
+                defense: 30
             }
         },
         {
@@ -129,9 +129,9 @@ export const PokedexContent = () => {
             color: 'orange',
             content: `It spits fire that is hot enough to melt boulders. It may cause forest fires by blowing flames.`,
             stats: {
-                hp: 50,
+                hp: 35,
                 attack: 50,
-                defense: 50
+                defense: 25
             }
         },
         {
@@ -140,9 +140,9 @@ export const PokedexContent = () => {
             color: 'blue',
             content: `Squirtle's shell is not merely used for protection. The shell's rounded shape and the grooves on its surface help minimize resistance in water, enabling this Pokémon to swim at high speeds.`,
             stats: {
-                hp: 50,
-                attack: 50,
-                defense: 50
+                hp: 20,
+                attack: 20,
+                defense: 30
             }
         },
         {
@@ -151,9 +151,9 @@ export const PokedexContent = () => {
             color: 'blue',
             content: `Its tail is large and covered with rich, thick fur. The tail becomes increasingly deeper in color as Wartortle ages. The scratches on its shell are evidence of this Pokémon's toughness as a battler.`,
             stats: {
-                hp: 50,
-                attack: 50,
-                defense: 50
+                hp: 30,
+                attack: 30,
+                defense: 40
             }
         },
         {
@@ -162,9 +162,9 @@ export const PokedexContent = () => {
             color: 'blue',
             content: `Blastoise has water spouts that protrude from its shell. The water spouts are very accurate. They can shoot bullets of water with enough accuracy to strike empty cans from adistance of over 160 feet.`,
             stats: {
-                hp: 50,
-                attack: 50,
-                defense: 50
+                hp: 25,
+                attack: 45,
+                defense: 60
             }
         }
     ];
