8e069e4a01a71ca5800c15dd6ba5175f046753f8
parav-jaikrishan <r21947parav@dpsrkp.net>
2022-08-21 00:27:32 +0530

    update

--- src/components/home-content/HomeContent.js
+++ src/components/home-content/HomeContent.js
@@ -26,6 +26,34 @@ export const HomeContent = () => {
                 quae architecto eius itaque vero officia cum nemo, veniam animi est reiciendis praesentium earum quis 
                 quibusdam nam voluptatum ipsum eos commodi! Sequi nobis dolorum eos atque, qui inventore cumque 
                 laudantium ducimus animi, minus accusamus, fugiat alias eveniet debitis consequatur ratione magnam fugit ab ipsa!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id aut laudantium, possimus dolor, soluta hic 
+                totam eaque neque magnam quidem fugiat ipsa expedita ab explicabo architecto vitae dicta animi illo 
+                temporibus odio. Hic cupiditate, assumenda natus consequatur quas consequuntur facere deserunt labore 
+                ex, provident quisquam vel repudiandae beatae sint! Qui provident quaerat rem explicabo mollitia quisquam 
+                quae architecto eius itaque vero officia cum nemo, veniam animi est reiciendis praesentium earum quis 
+                quibusdam nam voluptatum ipsum eos commodi! Sequi nobis dolorum eos atque, qui inventore cumque 
+                laudantium ducimus animi, minus accusamus, fugiat alias eveniet debitis consequatur ratione magnam fugit ab ipsa!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id aut laudantium, possimus dolor, soluta hic 
+                totam eaque neque magnam quidem fugiat ipsa expedita ab explicabo architecto vitae dicta animi illo 
+                temporibus odio. Hic cupiditate, assumenda natus consequatur quas consequuntur facere deserunt labore 
+                ex, provident quisquam vel repudiandae beatae sint! Qui provident quaerat rem explicabo mollitia quisquam 
+                quae architecto eius itaque vero officia cum nemo, veniam animi est reiciendis praesentium earum quis 
+                quibusdam nam voluptatum ipsum eos commodi! Sequi nobis dolorum eos atque, qui inventore cumque 
+                laudantium ducimus animi, minus accusamus, fugiat alias eveniet debitis consequatur ratione magnam fugit ab ipsa!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id aut laudantium, possimus dolor, soluta hic 
+                totam eaque neque magnam quidem fugiat ipsa expedita ab explicabo architecto vitae dicta animi illo 
+                temporibus odio. Hic cupiditate, assumenda natus consequatur quas consequuntur facere deserunt labore 
+                ex, provident quisquam vel repudiandae beatae sint! Qui provident quaerat rem explicabo mollitia quisquam 
+                quae architecto eius itaque vero officia cum nemo, veniam animi est reiciendis praesentium earum quis 
+                quibusdam nam voluptatum ipsum eos commodi! Sequi nobis dolorum eos atque, qui inventore cumque 
+                laudantium ducimus animi, minus accusamus, fugiat alias eveniet debitis consequatur ratione magnam fugit ab ipsa!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id aut laudantium, possimus dolor, soluta hic 
+                totam eaque neque magnam quidem fugiat ipsa expedita ab explicabo architecto vitae dicta animi illo 
+                temporibus odio. Hic cupiditate, assumenda natus consequatur quas consequuntur facere deserunt labore 
+                ex, provident quisquam vel repudiandae beatae sint! Qui provident quaerat rem explicabo mollitia quisquam 
+                quae architecto eius itaque vero officia cum nemo, veniam animi est reiciendis praesentium earum quis 
+                quibusdam nam voluptatum ipsum eos commodi! Sequi nobis dolorum eos atque, qui inventore cumque 
+                laudantium ducimus animi, minus accusamus, fugiat alias eveniet debitis consequatur ratione magnam fugit ab ipsa!</p>
         </div>
     );
 }
