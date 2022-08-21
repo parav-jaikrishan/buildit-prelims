922f935dad9617849aa38393b1a52259ceaa89b5
parav-jaikrishan <r21947parav@dpsrkp.net>
2022-08-22 00:53:47 +0530

    Video added

--- src/components/home-content/HomeContent.js
+++ src/components/home-content/HomeContent.js
@@ -5,55 +5,11 @@ export const HomeContent = () => {
     return (
         <div className="home-content">
             <h1>Home</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id aut laudantium, possimus dolor, soluta hic 
-                totam eaque neque magnam quidem fugiat ipsa expedita ab explicabo architecto vitae dicta animi illo 
-                temporibus odio. Hic cupiditate, assumenda natus consequatur quas consequuntur facere deserunt labore 
-                ex, provident quisquam vel repudiandae beatae sint! Qui provident quaerat rem explicabo mollitia quisquam 
-                quae architecto eius itaque vero officia cum nemo, veniam animi est reiciendis praesentium earum quis 
-                quibusdam nam voluptatum ipsum eos commodi! Sequi nobis dolorum eos atque, qui inventore cumque 
-                laudantium ducimus animi, minus accusamus, fugiat alias eveniet debitis consequatur ratione magnam fugit ab ipsa!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id aut laudantium, possimus dolor, soluta hic 
-                totam eaque neque magnam quidem fugiat ipsa expedita ab explicabo architecto vitae dicta animi illo 
-                temporibus odio. Hic cupiditate, assumenda natus consequatur quas consequuntur facere deserunt labore 
-                ex, provident quisquam vel repudiandae beatae sint! Qui provident quaerat rem explicabo mollitia quisquam 
-                quae architecto eius itaque vero officia cum nemo, veniam animi est reiciendis praesentium earum quis 
-                quibusdam nam voluptatum ipsum eos commodi! Sequi nobis dolorum eos atque, qui inventore cumque 
-                laudantium ducimus animi, minus accusamus, fugiat alias eveniet debitis consequatur ratione magnam fugit ab ipsa!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id aut laudantium, possimus dolor, soluta hic 
-                totam eaque neque magnam quidem fugiat ipsa expedita ab explicabo architecto vitae dicta animi illo 
-                temporibus odio. Hic cupiditate, assumenda natus consequatur quas consequuntur facere deserunt labore 
-                ex, provident quisquam vel repudiandae beatae sint! Qui provident quaerat rem explicabo mollitia quisquam 
-                quae architecto eius itaque vero officia cum nemo, veniam animi est reiciendis praesentium earum quis 
-                quibusdam nam voluptatum ipsum eos commodi! Sequi nobis dolorum eos atque, qui inventore cumque 
-                laudantium ducimus animi, minus accusamus, fugiat alias eveniet debitis consequatur ratione magnam fugit ab ipsa!</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id aut laudantium, possimus dolor, soluta hic 
-                totam eaque neque magnam quidem fugiat ipsa expedita ab explicabo architecto vitae dicta animi illo 
-                temporibus odio. Hic cupiditate, assumenda natus consequatur quas consequuntur facere deserunt labore 
-                ex, provident quisquam vel repudiandae beatae sint! Qui provident quaerat rem explicabo mollitia quisquam 
-                quae architecto eius itaque vero officia cum nemo, veniam animi est reiciendis praesentium earum quis 
-                quibusdam nam voluptatum ipsum eos commodi! Sequi nobis dolorum eos atque, qui inventore cumque 
-                laudantium ducimus animi, minus accusamus, fugiat alias eveniet debitis consequatur ratione magnam fugit ab ipsa!</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id aut laudantium, possimus dolor, soluta hic 
-                totam eaque neque magnam quidem fugiat ipsa expedita ab explicabo architecto vitae dicta animi illo 
-                temporibus odio. Hic cupiditate, assumenda natus consequatur quas consequuntur facere deserunt labore 
-                ex, provident quisquam vel repudiandae beatae sint! Qui provident quaerat rem explicabo mollitia quisquam 
-                quae architecto eius itaque vero officia cum nemo, veniam animi est reiciendis praesentium earum quis 
-                quibusdam nam voluptatum ipsum eos commodi! Sequi nobis dolorum eos atque, qui inventore cumque 
-                laudantium ducimus animi, minus accusamus, fugiat alias eveniet debitis consequatur ratione magnam fugit ab ipsa!</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id aut laudantium, possimus dolor, soluta hic 
-                totam eaque neque magnam quidem fugiat ipsa expedita ab explicabo architecto vitae dicta animi illo 
-                temporibus odio. Hic cupiditate, assumenda natus consequatur quas consequuntur facere deserunt labore 
-                ex, provident quisquam vel repudiandae beatae sint! Qui provident quaerat rem explicabo mollitia quisquam 
-                quae architecto eius itaque vero officia cum nemo, veniam animi est reiciendis praesentium earum quis 
-                quibusdam nam voluptatum ipsum eos commodi! Sequi nobis dolorum eos atque, qui inventore cumque 
-                laudantium ducimus animi, minus accusamus, fugiat alias eveniet debitis consequatur ratione magnam fugit ab ipsa!</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id aut laudantium, possimus dolor, soluta hic 
-                totam eaque neque magnam quidem fugiat ipsa expedita ab explicabo architecto vitae dicta animi illo 
-                temporibus odio. Hic cupiditate, assumenda natus consequatur quas consequuntur facere deserunt labore 
-                ex, provident quisquam vel repudiandae beatae sint! Qui provident quaerat rem explicabo mollitia quisquam 
-                quae architecto eius itaque vero officia cum nemo, veniam animi est reiciendis praesentium earum quis 
-                quibusdam nam voluptatum ipsum eos commodi! Sequi nobis dolorum eos atque, qui inventore cumque 
-                laudantium ducimus animi, minus accusamus, fugiat alias eveniet debitis consequatur ratione magnam fugit ab ipsa!</p>
+            <p>The time we have all been waiting for has finally come. Ash Ketchum is finally visiting India! Under 
+                the guidance of Doctor Oak, he has learnt about the new Pokémon species Qutubex, found only in the worn-out 
+                corners of the capital city of Delhi. It has been rumoured to be one of the most powerful rock type pokemons 
+                ever seen. To help him on his adventure, Ash teams up with X, and embarks on a journey to defeat the three Gym Leaders.</p>
+                <iframe src="https://www.youtube.com/embed/RVdME0VhnjE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
     );
 }
